23a5cdad23c86e35f07fc672a1fc498f075eb1e4
unknown <wisdomtoyin@gmail.com>
2026-08-24 14:30:37 +0100

    debug production JWT environment

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -4,6 +4,13 @@ import app from "./app";
 import { env } from "./config/env";
 import { initializeSocket } from "./socket";
 
+console.log("JWT ENV CHECK:", {
+  accessSecretExists: Boolean(process.env.JWT_ACCESS_SECRET),
+  refreshSecretExists: Boolean(process.env.JWT_REFRESH_SECRET),
+  accessSecretLength: process.env.JWT_ACCESS_SECRET?.length || 0,
+  refreshSecretLength: process.env.JWT_REFRESH_SECRET?.length || 0,
+});
+
 const server = http.createServer(app);
 
 initializeSocket(server);
